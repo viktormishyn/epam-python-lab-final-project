2d58925e620ef90e9d256b4fea29883f8f2ee70c
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-27 21:30:31 +0300

    Modify Game model (add genre field)

--- backend/django_store_project/store/admin.py
+++ backend/django_store_project/store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from store.models import Game
 
-# Register your models here.
+
+@admin.register(Game)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price', 'genre']
--- backend/django_store_project/store/models.py
+++ backend/django_store_project/store/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 
 class Game(models.Model):
+    RPG = 'RPG'
+    STR = 'Strategy'
+    ACT = 'Action'
+
+    GENRES = (
+        (RPG, 'RPG'),
+        (STR, 'Strategy'),
+        (ACT, 'Action'),
+    )
+
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    genre = models.CharField(max_length=20, choices=GENRES, null=True)
+
+    def __str__(self):
+        return self.name
