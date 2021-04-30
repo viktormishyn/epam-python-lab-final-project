451328334ebd5a4cf8903c2db450c54376ecc102
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-30 14:39:35 +0300

    Create mockup routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import React from "react"
+import './App.css'
+import { Routes, Route } from "react-router-dom"
+import { Whoops404 } from "./components/Whoops404"
+import { Games } from "./components/Games/Games"
 
 function App() {
   return (
-    
+    <div>
+      <Routes>
+        <Route path="/" element={<Games />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="*" element={<Whoops404 />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/components/Games/Games.js
@@ -0,0 +1,7 @@
+export function Games() {
+    return (
+        <div>
+            <h1>Games</h1>
+        </div>
+    )
+}
