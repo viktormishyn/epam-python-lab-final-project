3a0d337b457322374afa2b644f6ec146183151ad
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-02 13:00:08 +0300

    Create Header.js, Header.module.css;

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,5 +1,26 @@
+import s from "./Header.module.css";
+import logo from "../../static/logo.svg";
+
 function Header() {
-  return <h3>Header</h3>;
+  return (
+    <header className={s.header}>
+      <span className={s.header_logo}>
+        <img src={logo} alt="logo" />
+      </span>
+      <div className={s.header_left}>
+        <span>GameStore</span>
+      </div>
+      <div className={s.header_nav}>
+        <span>Games</span>
+        <span>Community</span>
+        <span>About</span>
+        <span>Support</span>
+      </div>
+      <div className={s.header_login}>
+        <span>Sing In</span>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
