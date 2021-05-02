acf53523eee26d1b2af4844504a861a68f11dc53
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-02 12:32:51 +0300

    Create Footer.js, Footer.module.css; Add logo.svg

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,5 +1,20 @@
+import s from "./Footer.module.css";
+import logo from "../../static/logo.svg";
+
 function Footer() {
-  return <h3>Footer</h3>;
+  return (
+    <footer className={s.footer}>
+      <span className={s.footer_logo}>
+        <img src={logo} alt="logo" />
+      </span>
+      <div className={s.footer_left}>
+        <span>GameStore</span>
+      </div>
+      <div className={s.footer_right}>
+        <span>Copyrights - 2021</span>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
