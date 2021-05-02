bfabb9d42b9573b129f8ff5031085eff4555e61e
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-02 10:47:12 +0300

    Create simple css layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Whoops404 } from "./components/Whoops404";
-import { Games } from "./components/Games/Games";
+import Games from "./components/Games/Games";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div>
+    <div className="app-wraper">
+      <Header />
       <Routes>
         <Route path="/" element={<Games />} />
         <Route path="/games" element={<Games />} />
         <Route path="*" element={<Whoops404 />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- frontend/src/components/Games/Games.js
+++ frontend/src/components/Games/Games.js
@@ -1,14 +1,15 @@
+import s from "./Games.module.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export function Games() {
+function Games() {
   const [games, setGames] = useState([]);
 
   useEffect(() => {
     axios
       .get("http://localhost:8000/api/games")
       .then((res) => {
-        console.log(res);
+        setGames(res.data);
       })
       .catch((err) => {
         console.log(err);
@@ -16,13 +17,20 @@ export function Games() {
   });
 
   return (
-    <div>
+    <div className={s.games}>
       <h1>Games</h1>
       <ul>
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <li key={game.id} style={{ float: "left", margin: "10px" }}>
+            <p>{game.name}</p>
+            <p>{game.genre}</p>
+            <p>{game.description}</p>
+            <p>{game.price}</p>
+          </li>
         ))}
       </ul>
     </div>
   );
 }
+
+export default Games;
