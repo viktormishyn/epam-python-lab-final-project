f63560aa8edbc96a5e9297d0d519f547b2965051
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-07 20:45:18 +0300

    Configure testing environment

--- backend/django_store_project/store/tests/test_models.py
+++ backend/django_store_project/store/tests/test_models.py
@@ -1,12 +1,13 @@
-# from mixer.backend.django import mixer
-# import pytest
+from mixer.backend.django import mixer
+import pytest
 
-# @pytest.mark.django_db
-# class TestModels:
 
-#     def test_price_is_valid(self):
-#         # price shouldn't have more than 5 digits including 2 decimal points (max value 999.00)
-#         game = mixer.blend('store.Game', price='999')
-#         assert game.price == 999.00
-#         with pytest.raises(Exception):
-#             game = mixer.blend('store.Game', price='1000')
+@pytest.mark.django_db
+class TestModels:
+
+    def test_price_is_valid(self):
+        # price shouldn't have more than 5 digits including 2 decimal points (max value 999.00)
+        game = mixer.blend('store.Game', price='999')
+        assert game.price == 999.00
+        with pytest.raises(Exception):
+            game = mixer.blend('store.Game', price='1000')
