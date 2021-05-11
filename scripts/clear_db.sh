aecdb009d72c19f532feae5a597b5d8399ff814e
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-11 21:50:11 +0300

    Update seed_db.sh and clear_db.sh

--- scripts/clear_db.sh
+++ scripts/clear_db.sh
@@ -1,2 +1,3 @@
 curl -X DELETE http://localhost:8000/api/v1/games/
+curl -X DELETE http://localhost:8000/api/v1/genres/
 rm -rf ./backend/django_store_project/images
--- scripts/seed_db.sh
+++ scripts/seed_db.sh
@@ -1,3 +1,10 @@
+declare -a arr=('Strategy' 'RPG' 'Action'
+    'Adventure' 'Simulation' 'Sports' 'Racing')
+
+for i in "${arr[@]}"; do
+    curl -X POST http://localhost:8000/api/v1/genres/ -F "name=$i"
+done
+
 curl -X POST http://localhost:8000/api/v1/games/ \
     -F "name=Warcraft III: The Frozen Throne" -F 'description=Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor erat id dapibus tristique. Integer fringilla tellus imperdiet, porta diam sit amet, egestas nisl. Proin euismod sit amet urna quis blandit. Donec ut nulla pulvinar, commodo ante eu, tincidunt massa. Aenean eu nisl blandit urna lacinia volutpat id eget dolor. Quisque id tortor ex. Sed lacinia dictum nisl eget porta. Aenean ultrices maximus risus, in pulvinar magna bibendum non. Cras ac tellus dapibus, efficitur diam nec, consequat justo.' \
     -F 'price=20.00' -F 'genre=Strategy' -F "image=@./backend/test_data/warcraft.jpg"
