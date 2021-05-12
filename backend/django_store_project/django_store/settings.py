ac0bb0f39846386ae9dcea3833f57908a1b48eb4
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-12 20:09:07 +0300

    Add permissions configuration to django_store.settings.py

--- backend/django_store_project/django_store/settings.py
+++ backend/django_store_project/django_store/settings.py
@@ -137,3 +137,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+        # 'rest_framework.permissions.IsAdminUser',
+        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
