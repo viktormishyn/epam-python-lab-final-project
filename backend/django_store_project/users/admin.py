8fb30297e8960620d6e3454ff11159cc9e5893ee
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-13 12:52:57 +0300

    Customize admin panel in users.admin

--- backend/django_store_project/users/admin.py
+++ backend/django_store_project/users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from users.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea, CharField
+from django import forms
+from django.db import models
 
-# Register your models here.
+
+class UserAdminConfig(UserAdmin):
+    model = User
+    search_fields = ('email', 'username')
+    list_filter = ('email', 'username', 'is_active', 'is_staff')
+    ordering = ('-start_date',)
+    list_display = ('email', 'id', 'username', 'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'username',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Personal', {'fields': ('about',)}),
+    )
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 20, 'cols': 60})},
+    }
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_active', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(User, UserAdminConfig)
