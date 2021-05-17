742c7c19eb95afd8b5244ec152b6cfad4c72a3e0
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-17 10:56:34 +0300

    Delete static files, change media files directory to 'media/'; update clear_db.sh script

--- scripts/clear_db.sh
+++ scripts/clear_db.sh
@@ -1,3 +1,3 @@
 curl -u "admin@gmail.com:admin" -X DELETE http://localhost:8000/api/v1/games/
 curl -u "admin@gmail.com:admin" -X DELETE http://localhost:8000/api/v1/genres/
-rm -rf ./backend/django_store_project/images
+rm -rf ./backend/django_store_project/media/images
