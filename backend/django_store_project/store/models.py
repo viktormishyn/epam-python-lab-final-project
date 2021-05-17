d63991f5d57f72460cff62aa330fbeaaac979fab
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-17 10:08:13 +0300

    Add date_added field to Game model

--- backend/django_store_project/store/models.py
+++ backend/django_store_project/store/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+    class Meta:
+        ordering = ('name',)
+
     def __str__(self):
         return self.name
 
@@ -17,6 +20,10 @@ class Game(models.Model):
     genre = models.ForeignKey(
         Genre, to_field='name', on_delete=models.PROTECT, null=True)
     image = models.ImageField(upload_to="images/", null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('date_added',)
 
     def __str__(self):
         return self.name
