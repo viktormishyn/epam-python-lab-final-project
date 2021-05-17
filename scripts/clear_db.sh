a364eb6451d0fb6bcedd715b93779a0a0d130bb1
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-17 10:03:16 +0300

    Add test credentials to clear_db.sh and seed_db.sh

--- scripts/clear_db.sh
+++ scripts/clear_db.sh
@@ -1,3 +1,3 @@
-curl -X DELETE http://localhost:8000/api/v1/games/
-curl -X DELETE http://localhost:8000/api/v1/genres/
+curl -u "admin@gmail.com:admin" -X DELETE http://localhost:8000/api/v1/games/
+curl -u "admin@gmail.com:admin" -X DELETE http://localhost:8000/api/v1/genres/
 rm -rf ./backend/django_store_project/images
--- scripts/seed_db.sh
+++ scripts/seed_db.sh
@@ -2,33 +2,33 @@ declare -a arr=('Strategy' 'RPG' 'Action'
     'Adventure' 'Simulation' 'Sports' 'Racing')
 
 for i in "${arr[@]}"; do
-    curl -X POST http://localhost:8000/api/v1/genres/ -F "name=$i"
+    curl -u "admin@gmail.com:admin" -X POST http://localhost:8000/api/v1/genres/ -F "name=$i"
 done
 
-curl -X POST http://localhost:8000/api/v1/games/ \
+curl -u "admin@gmail.com:admin" -X POST http://localhost:8000/api/v1/games/ \
     -F "name=Warcraft III: The Frozen Throne" -F 'description=Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor erat id dapibus tristique. Integer fringilla tellus imperdiet, porta diam sit amet, egestas nisl. Proin euismod sit amet urna quis blandit. Donec ut nulla pulvinar, commodo ante eu, tincidunt massa. Aenean eu nisl blandit urna lacinia volutpat id eget dolor. Quisque id tortor ex. Sed lacinia dictum nisl eget porta. Aenean ultrices maximus risus, in pulvinar magna bibendum non. Cras ac tellus dapibus, efficitur diam nec, consequat justo.' \
     -F 'price=20.00' -F 'genre=Strategy' -F "image=@./backend/test_data/warcraft.jpg"
 
-curl -X POST http://localhost:8000/api/v1/games/ \
+curl -u "admin@gmail.com:admin" -X POST http://localhost:8000/api/v1/games/ \
     -F "name=Counter-Strike: Source" -F 'description=Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor erat id dapibus tristique. Integer fringilla tellus imperdiet, porta diam sit amet, egestas nisl. Proin euismod sit amet urna quis blandit. Donec ut nulla pulvinar, commodo ante eu, tincidunt massa. Aenean eu nisl blandit urna lacinia volutpat id eget dolor. Quisque id tortor ex. Sed lacinia dictum nisl eget porta. Aenean ultrices maximus risus, in pulvinar magna bibendum non. Cras ac tellus dapibus, efficitur diam nec, consequat justo.' \
     -F 'price=20.00' -F 'genre=Action' -F "image=@./backend/test_data/cs.jpg"
 
-curl -X POST http://localhost:8000/api/v1/games/ \
+curl -u "admin@gmail.com:admin" -X POST http://localhost:8000/api/v1/games/ \
     -F "name=Gothic II: Gold Edition" -F 'description=Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor erat id dapibus tristique. Integer fringilla tellus imperdiet, porta diam sit amet, egestas nisl. Proin euismod sit amet urna quis blandit. Donec ut nulla pulvinar, commodo ante eu, tincidunt massa. Aenean eu nisl blandit urna lacinia volutpat id eget dolor. Quisque id tortor ex. Sed lacinia dictum nisl eget porta. Aenean ultrices maximus risus, in pulvinar magna bibendum non. Cras ac tellus dapibus, efficitur diam nec, consequat justo.' \
     -F 'price=59.99' -F 'genre=RPG' -F "image=@./backend/test_data/gothic.jpg"
 
-curl -X POST http://localhost:8000/api/v1/games/ \
+curl -u "admin@gmail.com:admin" -X POST http://localhost:8000/api/v1/games/ \
     -F "name=Sid Meier's Civilization V" -F 'description=Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor erat id dapibus tristique. Integer fringilla tellus imperdiet, porta diam sit amet, egestas nisl. Proin euismod sit amet urna quis blandit. Donec ut nulla pulvinar, commodo ante eu, tincidunt massa. Aenean eu nisl blandit urna lacinia volutpat id eget dolor. Quisque id tortor ex. Sed lacinia dictum nisl eget porta. Aenean ultrices maximus risus, in pulvinar magna bibendum non. Cras ac tellus dapibus, efficitur diam nec, consequat justo.' \
     -F 'price=79.99' -F 'genre=Strategy' -F "image=@./backend/test_data/civilization.jpg"
 
-curl -X POST http://localhost:8000/api/v1/games/ \
+curl -u "admin@gmail.com:admin" -X POST http://localhost:8000/api/v1/games/ \
     -F "name=The Elder Scrolls III: Morrowind" -F 'description=Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor erat id dapibus tristique. Integer fringilla tellus imperdiet, porta diam sit amet, egestas nisl. Proin euismod sit amet urna quis blandit. Donec ut nulla pulvinar, commodo ante eu, tincidunt massa. Aenean eu nisl blandit urna lacinia volutpat id eget dolor. Quisque id tortor ex. Sed lacinia dictum nisl eget porta. Aenean ultrices maximus risus, in pulvinar magna bibendum non. Cras ac tellus dapibus, efficitur diam nec, consequat justo.' \
     -F 'price=29.99' -F 'genre=RPG' -F "image=@./backend/test_data/morrowind.jpg"
 
-curl -X POST http://localhost:8000/api/v1/games/ \
+curl -u "admin@gmail.com:admin" -X POST http://localhost:8000/api/v1/games/ \
     -F "name=Unreal Tournament 3" -F 'description=Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor erat id dapibus tristique. Integer fringilla tellus imperdiet, porta diam sit amet, egestas nisl. Proin euismod sit amet urna quis blandit. Donec ut nulla pulvinar, commodo ante eu, tincidunt massa. Aenean eu nisl blandit urna lacinia volutpat id eget dolor. Quisque id tortor ex. Sed lacinia dictum nisl eget porta. Aenean ultrices maximus risus, in pulvinar magna bibendum non. Cras ac tellus dapibus, efficitur diam nec, consequat justo.' \
     -F 'price=35.99' -F 'genre=Action' -F "image=@./backend/test_data/unreal_tournament.jpg"
 
-curl -X POST http://localhost:8000/api/v1/games/ \
+curl -u "admin@gmail.com:admin" -X POST http://localhost:8000/api/v1/games/ \
     -F "name=The Witcher" -F 'description=Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor erat id dapibus tristique. Integer fringilla tellus imperdiet, porta diam sit amet, egestas nisl. Proin euismod sit amet urna quis blandit. Donec ut nulla pulvinar, commodo ante eu, tincidunt massa. Aenean eu nisl blandit urna lacinia volutpat id eget dolor. Quisque id tortor ex. Sed lacinia dictum nisl eget porta. Aenean ultrices maximus risus, in pulvinar magna bibendum non. Cras ac tellus dapibus, efficitur diam nec, consequat justo.' \
     -F 'price=29.99' -F 'genre=RPG' -F "image=@./backend/test_data/witcher.jpg"
