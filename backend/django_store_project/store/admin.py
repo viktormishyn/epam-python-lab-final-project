435cbf74af1ffeca0ead06b10579998892496788
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-20 15:20:02 +0300

    Add game images to django admin panel

--- backend/django_store_project/store/admin.py
+++ backend/django_store_project/store/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from store.models import Game, Genre
 
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'genre', 'added']
+    list_display = ['image_show', 'name', 'price', 'genre', 'added']
     list_filter = ['genre', 'added']
     list_editable = ['price', 'genre']
+
+    def image_show(self, obj):
+        if obj.thumbnail:
+            return mark_safe(f"<img src='{obj.thumbnail.url}' width='60'/>")
 
 
 @admin.register(Genre)
