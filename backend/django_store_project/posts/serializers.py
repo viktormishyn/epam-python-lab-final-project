877991533baa0559f2f3aa15f27a8eeb3e17c9eb
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-29 13:18:31 +0300

    Refactor posts.serializers

--- backend/django_store_project/posts/serializers.py
+++ backend/django_store_project/posts/serializers.py
@@ -1,26 +1,23 @@
 from rest_framework import serializers
 
 from .models import Post, PostReply
-# from store.serializers import GamePostSerializer
 from users.serializers import UserPostSerializer
 
 
 class PostReplySerializer(serializers.ModelSerializer):
 
-    # game = GamePostSerializer(read_only=True, many=False)
     author = UserPostSerializer(read_only=True, many=False)
 
     class Meta:
         model = PostReply
-        fields = ('id', 'author', 'created_at', 'content', 'edited')
+        fields = ('id', 'post', 'author', 'created_at', 'content', 'edited')
 
 
 class PostSerializer(serializers.ModelSerializer):
 
-    # game = GamePostSerializer(read_only=True, many=False)
     author = UserPostSerializer(read_only=True, many=False)
     get_replies = PostReplySerializer(many=True, required=False)
 
     class Meta:
         model = Post
-        fields = ('id', 'author', 'created_at', 'content', 'edited', 'get_replies')
+        fields = ('id', 'game', 'author', 'created_at', 'content', 'edited', 'get_replies')
