f0232a86585d125df52c3535c97f09227af86258
Viktor Mishyn <viktormishyn@gmail.com>
2021-05-30 13:29:52 +0300

    Register orders.models.OrderInfo in admin.py

--- backend/django_store_project/orders/admin.py
+++ backend/django_store_project/orders/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
-from orders.models import Order, OrderItem
+from orders.models import Order, OrderItem, OrderInfo
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['user', 'created_at', 'ref_code', 'ordered', 'ordered_at', 'get_items', 'get_total', 'order_info']
     list_filter = ['created_at']
+
+
+@admin.register(OrderInfo)
+class OrderInfoAdmin(admin.ModelAdmin):
+    list_display = ['order', 'first_name', 'last_name', 'email', 'phone', 'payment_type', 'comments']
 
 
 @admin.register(OrderItem)
